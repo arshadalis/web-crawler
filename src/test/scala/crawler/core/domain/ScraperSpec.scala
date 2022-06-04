0e6e87ccbd2072d8aed49977acffb1a4e73ddbe4
Arshadali Sayed <arshadalisayed@192.168.1.6>
2022-06-04 13:01:23 +0530

    TDD: Add scraper spec

--- src/test/scala/crawler/core/domain/ScraperSpec.scala
+++ src/test/scala/crawler/core/domain/ScraperSpec.scala
@@ -1,5 +1,27 @@
 package crawler.core.domain
 
-class ScraperSpec {
+import org.scalatest.flatspec.AnyFlatSpec
 
+class ScraperSpec extends AnyFlatSpec {
+
+
+  "Scraper" should "return return error if the web page is not valid html page" in {
+
+  }
+
+  "Scraper" should "return Scraped data if the web page provided is valid page " in {
+
+  }
+
+  it should "scrape title of the page if present" in {
+  }
+
+  it should "scrape Headings from the page" in {
+  }
+
+  it should "scrape Links from the page" in {
+  }
+
+  it should "scrape Media from the page" in {
+  }
 }
