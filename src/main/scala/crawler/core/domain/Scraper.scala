01a87a0456ebb054b881d9578b58d73c64adb6ea
Arshadali Sayed <arshadalisayed@192.168.1.3>
2022-06-05 13:01:42 +0530

    Add scraper buisness logic

--- src/main/scala/crawler/core/domain/Scraper.scala
+++ src/main/scala/crawler/core/domain/Scraper.scala
@@ -2,6 +2,11 @@ package crawler.core.domain
 
 import crawler.core.domain.error.CrawlerErrors.{ErrorMessage, InvalidWebPage, ScrapingError}
 import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+
+import scala.collection.JavaConverters._
+import java.lang
+import scala.reflect.internal.Variance.Extractor
 
 object Scraper {
 
@@ -9,17 +14,52 @@ object Scraper {
     def scrape(HTMLDocument: Document): Either[ScrapingError, ScrapedData]
   }
 
-  object ScraperImpl extends Scraper{
-    override def scrape(HTMLDocument: Document): Either[ScrapingError, ScrapedData] =
-      Left(InvalidWebPage(ErrorMessage("No implementation")))
+  object ScraperImpl extends Scraper {
+    override def scrape(document: Document): Either[ScrapingError, ScrapedData] = {
+
+      val mayBeTitle = extractNonEmptyString(document, document => document.title()).map(Title)
+      val mayBeBody = extractNonEmptyString(document, document => document.body().data()).map(Body)
+
+      val mayBeHeadings = extractFromNonEmptyList(
+        extractElements(document, "h[1-6]"),
+        headingTags => Headings(headingTags)
+      )
+
+      val mayBeLinks = extractFromNonEmptyList(
+        extractElements(document, "a[href]"),
+        linksTags => Links(linksTags)
+      )
+
+      val mayBeMedia = extractFromNonEmptyList(
+        extractElements(document, "[src]"),
+        mediaTags => Media(mediaTags)
+      )
+
+      Right(ScrapedData(mayBeTitle, mayBeBody, mayBeHeadings, mayBeLinks, mayBeMedia))
+    }
+
+    private def extractNonEmptyString[T <: lang.String](document: Document, extractor: Document => T) = {
+      val data = extractor(document)
+      if (data != "") Some(data) else None
+    }
+
+    private def extractFromNonEmptyList[T](list: List[String], func: List[String] => T): Option[T] = list match {
+      case Nil => None
+      case list => Some(func(list))
+    }
+
+    private def extractElements(document: Document, extractorQuery: String) =
+      document.select(extractorQuery).eachText().asScala.toList
+
+
   }
 
   case class ScrapedData(
                           title: Option[Title],
-                          body: Option[Title],
-                          headings: Option[Title],
-                          links: Option[Title],
-                          media: Option[Title],
+                          body: Option[Body],
+                          headings: Option[Headings],
+                          links: Option[Links],
+                          media: Option[Media]
                         )
 
   case class Title(title: String) extends AnyVal
--- src/test/scala/crawler/core/domain/ScraperSpec.scala
+++ src/test/scala/crawler/core/domain/ScraperSpec.scala
@@ -1,27 +1,45 @@
 package crawler.core.domain
 
+import crawler.core.domain.Scraper.ScrapedData
+import crawler.core.domain.error.CrawlerErrors.{ErrorMessage, InvalidWebPage, ScrapingError}
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 class ScraperSpec extends AnyFlatSpec {
+  private val scraperImpl = Scraper.ScraperImpl
 
+  val mediumBlogUrl = "https://medium.com/kodeyoga/variance-is-not-hard-it-is-extremely-useful-f791b09e1f1c"
+  val blogDocument = Jsoup.connect(mediumBlogUrl).get()
+  private val scrapedData: Either[ScrapingError, ScrapedData] = scraperImpl.scrape(blogDocument)
 
   "Scraper" should "return return error if the web page is not valid html page" in {
-
+    val document = new Document("dummyUrl")
+    scraperImpl.scrape(document) shouldEqual Left(InvalidWebPage(ErrorMessage("No implementation")))
   }
 
   "Scraper" should "return Scraped data if the web page provided is valid page " in {
-
+    scrapedData.isRight shouldEqual true
   }
 
   it should "scrape title of the page if present" in {
+    val assertionResult = scrapedData.fold(_ => false, scrapedData => scrapedData.title.get.title shouldEqual ("abc"))
+    assertionResult shouldEqual true
   }
 
   it should "scrape Headings from the page" in {
+    val assertionResult = scrapedData.fold(_ => false, scrapedData => scrapedData.headings.get.headings.nonEmpty)
+    assertionResult shouldEqual true
   }
 
   it should "scrape Links from the page" in {
+    val assertionResult = scrapedData.fold(_ => false, scrapedData => scrapedData.links.get.links.nonEmpty)
+    assertionResult shouldEqual true
   }
 
   it should "scrape Media from the page" in {
+    val assertionResult = scrapedData.fold(_ => false, scrapedData => scrapedData.media.get.media.nonEmpty)
+    assertionResult shouldEqual true
   }
 }
